aa7fc28bebcb628ce53cc8700f79195ed6fa4f7e
Sander124 <85620656+Sander124@users.noreply.github.com>
2025-04-11 21:15:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def get_mongodb_connection():
     try:
         client = MongoClient(mongo_uri)
         db = client["pvs_db"]
+        st.write(db)
         collection = db["pvs_db"]
         return collection
     except Exception as e:
